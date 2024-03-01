bd1dbe488639e2b3528098a4a9b7a38059fa0540
hakansaglam <hakan.saglam@qconnex.com>
2024-03-01 11:48:06 +0100

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import styles from "../styles/home.module.css"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
